ec4c2be4180dfb480de6da9899cb128c112b43a2
taijiro72 <83691654+taijiro72@users.noreply.github.com>
2021-07-07 12:07:56 +0900

    JSS: Navbarmenu-click

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });  
+
+// Handle scrolling when tapping on the navbar menu const 상수
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth'});
+
+});
+  
