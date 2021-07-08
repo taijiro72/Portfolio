a7a6e07be3d1cc5653dbe6e4d67415dfc7a84750
taijiro72 <83691654+taijiro72@users.noreply.github.com>
2021-07-08 16:36:59 +0900

    JS: NavbarMenu_toggle

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -10,6 +11,8 @@ document.addEventListener('scroll', () => {
   }
 });  
 
+
+
 // Handle scrolling when tapping on the navbar menu const 상수
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -18,8 +21,18 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
@@ -58,7 +71,16 @@ workBtnContainer.addEventListener('click', (e) => {
   if(filter == null) {
     return;
   }
-  projectContainer.classList.add('anim-out');
+
+// Remove selection for the previous item and selec the new one
+  const active = document.querySelector('.category__btn.selected')
+  active.classList.remove('selected');
+  const target = 
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
+
+projectContainer.classList.add('anim-out');
   setTimeout(()=> {
     project.forEach((project) => {
       console.log(project.dataset.type);
